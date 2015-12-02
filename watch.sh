e322255f934984465b0ef76ffbdbfce1ddb11acd
Hellfar <pierre.louchet@gmail.com>
2015-12-02 03:44:10 +0100

    Removing the wrong files.

--- watch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ -z "`grep -s \"baseurl:\" _local_config.yml`" ]
-	then
-		echo "baseurl: \"`pwd`/_site\"" >> _local_config.yml
-fi
-
-jekyll build --watch --config _config.yml,_local_config.yml
